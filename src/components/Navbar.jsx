6a524c057d3a9c875631ae2a1592ebbefc909c9b
DeniseKelley <66845312+DeniseKelley@users.noreply.github.com>
2022-02-16 20:19:22 +0500

    created a Drink Menu.jsx file and function

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import Drink_Menu from '../pages/Drink_Menu/Drink_Menu';
 
 
 function NavBar() {
@@ -9,7 +9,7 @@ function NavBar() {
                 <div className = "logo">Pancakes</div>
                 <ul className = "nav-links">
                     <li><a href= "https://www.google.com/"> Food menu </a></li>
-                    <li><a> Drink menu </a></li>
+                    <li><a>Drink menu </a></li>
                     <li><a> Order Now!</a></li>
                 </ul>
             </nav>
---
+++ src/pages/Drink_Menu/Drink_Menu.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import {useEffect} from 'react'
+//Utilizing foodmenu_css 
+
+
+export default function Drink_Menu({drinkName}) {
+    
+    return (
+        <div>
+            <p>{drinkName}</p>
+        </div>
+    )
+}
