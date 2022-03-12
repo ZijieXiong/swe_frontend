40ce8d776ecec7a8c5fb24d55d391cbfcc8cb4a2
Tahmidur_Rabb <tr1476@nyu.edu>
2022-03-11 20:38:41 -0500

    Added order component

--- src/App.js
+++ src/App.js
@@ -4,13 +4,15 @@ import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import Home from './pages/Home/Home';
 import Rooms from './pages/Rooms/Rooms';
 import Users from './pages/Users/Users';
-import NavBar from './components/Navbar'
-import Footer from './components/Footer'
+import NavBar from './components/Navbar';
+import Footer from './components/Footer';
 import Food_Menu from './pages/Food_Menu/Food_Menu';
 import './App.css';
 import Drink_Menu from './pages/Drink_Menu/Drink_Menu';
 import LogIn from './pages/LogIn/LogIn';
 import About from './pages/About/About';
+import Order from './pages/Order/Order';
+
 
 function App() {
   return (
@@ -33,6 +35,9 @@ function App() {
             </Route>
             <Route exact={true} path={'/food_menu'}>
               <Food_Menu />
+            </Route>
+            <Route exact={true} path= {'/order'}>
+              <Order />
             </Route>
             <Route exact={true} path = {'/About'}>
               <About />
---
+++ src/pages/Order/Order.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Order() {
+    return (
+        <>
+            <div className = "order_menu" >
+                <p> Testing </p>
+            </div>
+        </>
+    )
+}
