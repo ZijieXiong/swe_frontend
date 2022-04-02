1072566efdbbf596fcfbf8f4a638adbb55078d54
Tahmidur_Rabb <tr1476@nyu.edu>
2022-04-01 22:17:04 -0400

    quick fixes to drink menu

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -5,9 +5,8 @@ export default function About() {
     return(
         <>
         <div className = "About_section">
-            <h1 id = "Title" >About us</h1>
 
-
+            <h1 id = "About_Title" >About us </h1>
 
             <p> Idea born with the interests of 5 intellectual students, we serve to provide the best quality food and the best service to our customer  </p>
 
