10e806c2ee8faec03b69a01f18ac14913aa2f6dc
Tahmidur_Rabb <tr1476@nyu.edu>
2022-04-16 15:04:47 -0400

    Adding functionality with login page

--- src/pages/LogIn/LogIn.jsx
+++ src/pages/LogIn/LogIn.jsx
@@ -2,28 +2,60 @@ import React, {useState} from "react";
 import ReactDom from "react-dom";
 import "./Login.css";
 
+export default function Login() {
 
-const [errormessage, seterrormessage] = useState({});
-const [checksubmission, setchecksubmission] = useState(false);
+    //States 
+    const [errormessage, seterrormessage] = useState({});
+    const [checksubmission, setchecksubmission] = useState(false);
 
-const displayErrorMessage = (name) => 
+    //Temp database 
+    const database  = [
+        {
+            username: 'user1',
+            password: 'password1'
+        },
+        {
+            username: 'user2',
+            password: 'password2'
+        }
+    ];
+
+    const errors = {
+        user: "invalid username",
+        passwd: "invalid password"
+    };
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+
+        var {user, passwd} = document.forms[0]; 
+
+        const userInfo = database.find((user) => user.username === user.value);
+
+        if (userInfo) {
+            if(userInfo.password != passwd.value) {
+                displayErrorMessage({name: "pass", message: errors.pass});
+
+            }
+
+            else {
+                setchecksubmission(true);
+            }
+        }
+        else {
+            seterrormessage({name: 'user', message: errors.user});
+        }
+    };
+
+    const displayErrorMessage = (name) => 
     name === errormessage.name && ( 
         <div className = "error"> {errormessage.message} 
         </div>
     );
 
-const handleSubmit = (event) => {
-    event.preventDefault();
-};
-
-export default function Login() {
-    return (
-        <>
-        <div>
-            <form>
-           
-            <div className= "login_main"> 
-                <form> 
+    const LoginForm = (
+        <div className= "login_main"> 
+                <form onSubmit = {handleSubmit}>
                     <div id ="sign-up-form">
                         <div id = "title"> <h1> Login </h1> </div>
                         <div id = "gap"></div>
@@ -45,10 +77,14 @@ export default function Login() {
 
             </div>
 
-            
-            </form>
-            </div>
 
+    );
+
+    
+    return (
+        <> 
+            <p> Login </p>
+            {checksubmission ? <div>Successfully logged in</div>: LoginForm}
         </>
     );
 }
