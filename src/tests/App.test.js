89da835fd425854971c27881b12c9d0626577f59
DeniseKelley <66845312+DeniseKelley@users.noreply.github.com>
2022-05-05 18:04:12 +0500

    fixed the app test and moved it into the tests folder

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,5 +1,5 @@
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import App from '../App';
 import ReactDOM from 'react-dom';
 import { MemoryRouter } from 'react-router-dom';
 
