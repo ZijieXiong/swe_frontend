5b1e9ce1c2ae8f6513dc40ed39cda28cb732ba33
jhw8841 <jhw8841@nyu.edu>
2022-05-05 21:40:53 -0400

    FOOD_MENU ERROR, THIS VERSION DOES NOT WORK

--- src/pages/Food_Menu/Food_Menu.jsx
+++ src/pages/Food_Menu/Food_Menu.jsx
@@ -5,6 +5,7 @@ import axios from 'axios' //to use axios run "npm install axios"  in terminal
 import { useState, useEffect } from 'react';
 
 
+
 export default function Food_Menu({foodName}) {
 
     const [foodType, setFoodType] = useState([])
@@ -20,26 +21,11 @@ export default function Food_Menu({foodName}) {
         
         }, [] );
 
+    const[app_fooditems, setAppFoodItems] = useState(undefined)
+    const[ent_fooditems, setEntFoodItems] = useState(undefined)
+    const[des_fooditems, setDesFoodItems] = useState(undefined)
+    const[error, setError] = useState('')
 
-
-
-    const [foodItems, setFoodItems] = useState([])
-    const [error, setError] = useState('')
-    
-    // useEffect(() => {
-    //     axios.get('https://swe-temp.herokuapp.com/food_menu/list')
-    //     .then((res) => {
-    //         if (res.data) {
-    //            const formattedData = Object.values(res.data)
-    //              setFoodItems(formattedData);
-    //         }
-    //     })      
-       
-    //     .catch((err) => {
-    //         console.log(err);
-    //         setError(err.toString());
-    //     })
-    // }, [])
     useEffect(() => {
         axios.get('https://swe-temp.herokuapp.com/food_menu/list')
         .then((res) => {
@@ -47,18 +33,27 @@ export default function Food_Menu({foodName}) {
             //if(res.data) {
            // const beverages = Object.value(res.data.Alcoholic.Beer)
            //const beverages = Object.entries(res.data.Alcoholic.Beer)
-           const foods = [] 
+           const app_food = []
+	   const ent_food = []
+	   const des_food = []
            //console.log((res.data.Beer))
            const food = res.data.Appetizer
            // do foreach
            Object.keys(res.data.Appetizer).forEach(food=>{
-           console.log((food, res.data.Appetizer[food] ))
+           app_food.push({foodName: res.data.Appetizer[food].foodName, price: res.data.Appetizer[food].price, description: res.data.Appetizer[food].description})
+           console.log(app_food) })
 
-           
-           foods.push({foodName: res.data.Appetizer[food].foodName , price: res.data.Appetizer[food].price})
-           console.log(foods)
-           })
-           setFoodItems(foods)
+	   Object.keys(res.data.Entree).forEach(food=>{
+	   ent_food.push({foodName: res.data.Entree[food].foodName, price: res.data.Entree[food].price, description: res.data.Entree[food].description})
+	   console.log(ent_food) })
+
+	   Object.keys(res.data.Dessert).forEach(food=>{
+           des_food.push({foodName: res.data.Dessert[food].foodName, price: res.data.Dessert[food].price, description: res.data.Dessert[food].description})
+	   console.log(des_food) })
+
+	   setAppFoodItems(app_food)
+	   setEntFoodItems(ent_food)
+	   setDesFoodItems(des_food)
         })
         
 
@@ -67,145 +62,52 @@ export default function Food_Menu({foodName}) {
     return (
         <>
 
-
         <div id = "foodmenu_top">
-
-            <h1>  Food menu </h1>
-            <h2> Take a look at what we have to offer! </h2>
+	<h1>  Food menu </h1>
         </div>
+	
+	<div>
+        {foodType.map((type) => {
+	   
+	   let foodItems;
+	   
+	   switch (type.typeName) {
+	      case "Appetizer":
+		 foodItems = app_fooditems
+	         break;
 
-        {foodType.map((type) => (
-                   
-                   <h1> <p key={type.typeName}>{type.typeName} </p></h1>
-                   ))}
+	      case "Entree":
+		 foodItems = ent_fooditems
+	         break;
 
-        <h1> Appetizers</h1>
-        <section class = "grid_container">
-            <div className = "fooditems"> 
-                <h1 class = "foodname"> Food item </h1>
-
-                <h3 class = "ingredients"> Ingredients: </h3>
-
-                <p class = "price">  Price: </p>
-            </div>
-            
-            <div className = "fooditems"> 
-                <h1 class = "foodname"> Food item </h1>
-
-                <h3 class = "ingredients"> Ingredients: </h3>
-
-                <p class = "price">  Price: </p>
-            </div>
-
-            <div className = "fooditems"> 
-                <h1 class = "foodname"> Food item </h1>
-
-                <h3 class = "ingredients"> Ingredients: </h3>
-
-                <p class = "price">  Price: </p>
-            </div>
-            
-            <div className = "fooditems"> 
-                <h1 class = "foodname"> Food item </h1>
-
-                <h3 class = "ingredients"> Ingredients: </h3>
-
-                <p class = "price">  Price: </p>
-            </div>
-
-            <div className = "fooditems"> 
-                <h1 class = "foodname"> Food item </h1>
-
-                <h3 class = "ingredients"> Ingredients: </h3>
-
-                <p class = "price">  Price: </p>
-            </div>
-
-            <div className = "fooditems"> 
-                <h1 class = "foodname"> Food item </h1>
-
-                <h3 class = "ingredients"> Ingredients: </h3>
-
-                <p class = "price">  Price: </p>
-            </div>
-            </section>
-
-        <h1> Main courses </h1>
-        <section class = "grid_container">
-            <div className = "fooditems"> 
-                <h1 class = "foodname"> Food item </h1>
-
-                <h3 class = "ingredients"> Ingredients: </h3>
-
-                <p class = "price">  Price: </p>
-            </div>
-            
-            <div className = "fooditems"> 
-                <h1 class = "foodname"> Food item </h1>
-
-                <h3 class = "ingredients"> Ingredients: </h3>
-
-                <p class = "price">  Price: </p>
-            </div>
-
-            <div className = "fooditems"> 
-                <h1 class = "foodname"> Food item </h1>
-
-                <h3 class = "ingredients"> Ingredients: </h3>
-
-                <p class = "price">  Price: </p>
-            </div>
-            
-            <div className = "fooditems"> 
-                <h1 class = "foodname"> Food item </h1>
-
-                <h3 class = "ingredients"> Ingredients: </h3>
-
-                <p class = "price">  Price: </p>
-            </div>
-
-            <div className = "fooditems"> 
-                <h1 class = "foodname"> Food item </h1>
-
-                <h3 class = "ingredients"> Ingredients: </h3>
-
-                <p class = "price">  Price: </p>
-            </div>
-
-            <div className = "fooditems"> 
-                <h1 class = "foodname"> Food item </h1>
-
-                <h3 class = "ingredients"> Ingredients: </h3>
-
-                <p class = "price">  Price: </p>
-            </div>
-            </section>
-
-
-        <h1> Sides</h1>
-
-
-
-
-        <div>
-        {foodItems.map((product) => (
-                   <section class = "grid_container">
-                   <div className = "fooditems"> 
-                       <h1 class = "foodname"> Food item </h1>
-                       <p key={product.foodName}>{product.foodName} </p>
-                       <h3 class = "ingredients"> Ingredients: </h3>
-       
-                       <p class = "price">  Price: {product.price}  </p>
-                      
-                   </div>
-                   </section>
-      ))}
-          
-        </div>
-        </>
-
+	      case "Dessert": 
+		 foodItems = des_fooditems
+		 break;
+	   }
+	return (
         
+	<div>
+           <p key={type.typeName}> {type.typeName} </p>
+	   <div>
+	   <section class = "grid_container">
+	{foodItems && foodItems.map((item) => (
+		<div key={item.foodName}>
+		   <div className = "fooditems">
 
+		   <h1 class = "foodName"> {item.drinkName}</h1>
+		   <p class = "price"> Description: {item.description}</p>
+		   <p class = "price"> Price: ${item.price} </p>
+
+		</div>
+		</div>
+
+        	))}
+		</section>
+	</div>
+	   </div>
+	    )
+	</div>
+	})}
     )
 }
 
